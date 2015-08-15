3361dda17fd691de2fd1fa67ab41e8c2f7211b0c
Vlad Radulescu <pacMakaveli90@gmail.co.uk>
2015-08-15 22:56:54 +0100

    add platform api search

---
+++ lib/giantbomb/platform.rb
@@ -0,0 +1,29 @@
+module GiantBomb
+  class Platform < Resource
+    has_resource 'platform', plural: 'platforms', id: '3045'
+
+    # http://www.giantbomb.com/api/documentation#toc-0-26
+    #
+    @@fields = [
+      :abbreviation,      # Abbreviation of the platform.
+      :api_detail_url,    # URL pointing to the platform detail resource
+      :company,           # Company that created the platform.
+      :date_added,        # Date the platform was added to Giant Bomb
+      :date_last_updated, # Date the platform was last updated on Giant Bomb
+      :deck,              # Brief summary of the platform
+      :description,       # Description of the platform
+      :id,                # Unique ID of the platform
+      :image,             # Main Image of the platform
+      :install_base,      # Approximate number of sold hardware units.
+      :name,              # Name of the platform
+      :online_support,    # Flag indicating whether the platform has online capabilities.
+      :original_price,    # Initial price point of the platform.
+      :release_date,      # Date of the platform
+      :site_detail_url,   # URL pointing to the platform on Giant Bomb.
+    ]
+
+    @@fields.each do |field|
+      attr_accessor field
+    end
+  end
+end
